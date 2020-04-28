a6aeb1acdb617a401ddb004da17d8912be2a936c
nikhathfirdose <nikhathfirdose@github.com>
2020-04-28 20:44:00 +0530

    flask setup using redirect

--- servervenv/server.py
+++ servervenv/server.py
@@ -1,5 +1,5 @@
 # render allows us to display / return the html  file on entering route"/"
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 # Note debug mode need to be switched on in order to prevent usage of "flask run" ecah time changes are made
@@ -10,3 +10,18 @@ app = Flask(__name__)
 @app.route('/')  # decorator - helps us build routs in the server
 def portfolioPage():
     return render_template("index.html")
+
+
+@app.route('/<string:page_name>')  # dynamic page calling
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route("/submit_details", methods=['POST', 'GET'])
+def sendData():
+    if request.method == "POST":
+        # data = request.form["email"] , use this or just make a dictionary
+        data = request.form.to_dict()
+        return redirect("/thankyou.html")
+    else:
+        return "Something Went wrong"
