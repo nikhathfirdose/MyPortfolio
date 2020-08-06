94558b2133824736c96fde0cc12cb12659135b00
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-08-06 15:26:33 +0530

    fonts updated home page complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
-
 import "../index.css";
-function Home() {
-  return <div className="home-container"></div>;
-}
+const Home = () => {
+  return (
+    <div>
+      <header className="header-container">
+        <h1 className="heading">Nikhath Firdose</h1>
+        <div className="nav-bar">
+          <a href="#"> About Me </a>
+          <a href="#"> Projects</a>
+          <a href="#"> Resume </a>
+          <a href="#"> Contact </a>
+        </div>
+      </header>
+      <div className="cover-page">
+        <div className="introduction">
+          <h1 className="name"> Hey!, I am Nikhath Firdose</h1>
+          <p className="description"> i am a web devdeloper</p>
+        </div>
+      </div>
+      <div className="media-links"> social media</div>
+    </div>
+  );
+};
 
 export default Home;
