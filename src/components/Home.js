9774fafca5367823ed40a8ade3ae522b77bac2c0
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-08-07 14:04:33 +0530

    social media links added, created new animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,57 @@ const Home = () => {
           <p className="description"> i am a web devdeloper</p>
         </div>
       </div>
-      <div className="media-links"> social media</div>
+      <div className="media-links">
+        <a
+          id="profile-link"
+          href="https://github.com/nikhathfirdose"
+          target="_blank"
+        >
+          <img
+            src="https://i.postimg.cc/sD5PRDMy/Programming-Github-icon.png"
+            alt="github"
+          />
+        </a>
+        <a
+          id="profile-link"
+          href="https://twitter.com/nikhath_firdose"
+          target="_blank"
+        >
+          <img
+            src="https://i.postimg.cc/8cJSrvgF/Social-Networks-Twitter-icon.png"
+            alt="twitter"
+          />
+        </a>
+
+        <a
+          id="profile-link"
+          href="https://www.linkedin.com/in/nikhath-firdose/"
+          target="_blank"
+        >
+          <img src="https://i.postimg.cc/8zrSCMBx/Social-Networks-Linkedin-icon.png" />
+        </a>
+        <a
+          id="profile-link"
+          href="https://www.facebook.com/profile.php?id=100013857487670"
+          target="_blank"
+        >
+          <img
+            src="https://i.postimg.cc/fywztyTm/Social-Networks-Facebook-icon.png"
+            alt="facebook"
+          />
+        </a>
+
+        <a
+          id="profile-link"
+          href="https://www.instagram.com/nikhaaathh/"
+          target="_blank"
+        >
+          <img
+            src="https://i.postimg.cc/66GLYQct/Social-Networks-Instagram-icon-1.png"
+            alt="instagram"
+          />
+        </a>
+      </div>
     </div>
   );
 };
