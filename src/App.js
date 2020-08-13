6edcc0500c13084de6441c5a4327502e8f71a55e
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-08-13 15:44:51 +0530

    margins edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 const App = () => {
-  useEffect(() => {
-    document.title = "Nikhath Firdose";
-  }, []);
   return (
     <div>
       <Home />
