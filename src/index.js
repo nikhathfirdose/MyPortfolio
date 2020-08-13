71afabb392f8b6f9e000bd04d60426ba994f7c05
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-08-13 14:32:14 +0530

    title changed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
-    <About />
-    <Projects />
-    <Contact />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
