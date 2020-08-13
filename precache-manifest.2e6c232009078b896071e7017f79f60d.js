8949603e1ba576c735546b1ce90497edcd49e605
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-08-13 14:05:21 +0530

    Updates

---
+++ precache-manifest.2e6c232009078b896071e7017f79f60d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4875e9e9d4fb95594ec83b4a3ece3c1",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "5599da5c73a07d942505",
+    "url": "/MyPortfolio/static/css/main.8645e892.chunk.css"
+  },
+  {
+    "revision": "b871010ae62e131a38bf",
+    "url": "/MyPortfolio/static/js/2.dceb11c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.dceb11c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5599da5c73a07d942505",
+    "url": "/MyPortfolio/static/js/main.e1a12293.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  },
+  {
+    "revision": "0cf2128b80ccb2810d4a7477f43e1401",
+    "url": "/MyPortfolio/static/media/Game.0cf2128b.png"
+  },
+  {
+    "revision": "c3f8aad7ffbe210d38eac90f297fe783",
+    "url": "/MyPortfolio/static/media/Nikhath Firdose.c3f8aad7.pdf"
+  },
+  {
+    "revision": "1ff7a47aabddce5d8966a50f3eb5da4c",
+    "url": "/MyPortfolio/static/media/Quiz.1ff7a47a.png"
+  },
+  {
+    "revision": "a9552134c65921beac3e47d523be8179",
+    "url": "/MyPortfolio/static/media/computer-table.a9552134.jpeg"
+  },
+  {
+    "revision": "301828bd4f6c396b378cc001f9bb614c",
+    "url": "/MyPortfolio/static/media/picture.301828bd.jpg"
+  },
+  {
+    "revision": "59382976fb3cc414a3d00d7e2a6e6d07",
+    "url": "/MyPortfolio/static/media/shopPhone.59382976.gif"
+  },
+  {
+    "revision": "3f87a661dc917d1bf25cd45ee659d6fc",
+    "url": "/MyPortfolio/static/media/shopResize.3f87a661.gif"
+  }
+]);
